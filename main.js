d6a15855fb15191d203b5977be262949b434c2b9
carlos <continicarlos.alb@gmail.com>
2022-11-14 20:29:23 -0300

    primera entrega

---
+++ main.js
@@ -0,0 +1,45 @@
+
+let producto = ""
+let precio = 0
+let cantidad = 0
+let precioTotal = 0
+let cantidadTotal = 0
+let comprar = false;
+
+do{
+    producto = prompt("¿Que deseas comprar?");
+    cantidad = parseInt(prompt("¿Cuantos deseas comprar?"));
+
+
+
+    switch (producto){
+        case "gorra":
+            precio = 700;
+            break;
+        case "jogger":
+            precio = 2000;
+            break;
+        case "remera":
+            precio = 1000;
+            break;
+        case "zapatillas":
+            precio = 5000;
+            break;
+        default:
+            alert("Datos invalidos")
+            precio = 0;
+            cantidad = 0;
+    }
+    precioTotal = precio*cantidad;
+    cantidadTotal = cantidad;
+
+    comprar = confirm("¿Queres agregar otro producto?");
+
+} while(comprar);
+
+alert("A comprado " +cantidadTotal+ " productos y el total es $" +precioTotal);
+
+let terminar = confirm("Desea finalizar la compra?");
+if (terminar) alert("Su compra se ha reaizad con exito");
+if (!terminar) alert("Esperamos volver a verlo pronto!");
+
